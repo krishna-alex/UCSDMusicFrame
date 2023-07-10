b2933c69ef63a8ca9db549b86a51d3d4cc45842d
Krishna Alex <krishna.aromal@gmail.com>
2023-07-09 22:04:34 -0700

    Initial commit

--- MusicWireframe/ViewController.swift
+++ MusicWireframe/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var forwardBackground: UIView!
+    @IBOutlet weak var playPauseBackground: UIView!
+    @IBOutlet weak var reverseBackground: UIView!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var reverseButton: UIButton!
+    
+    @IBOutlet weak var forwardButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        reverseBackground.layer.cornerRadius = reverseBackground.frame.height / 2
+        reverseBackground.clipsToBounds = true
+        
+        playPauseBackground.layer.cornerRadius = playPauseBackground.frame.height / 2
+        playPauseBackground.clipsToBounds = true
+        
+       forwardBackground.layer.cornerRadius = forwardBackground.frame.height / 2
+       forwardBackground.clipsToBounds = true
     }
 
 
